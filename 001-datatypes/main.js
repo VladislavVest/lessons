4b13291df53be013eb3f80b1fc557a929363f807
VladislavVest <vladislavbrusencov@gmail.com>
2023-07-03 16:59:54 +0300

    lesson1

---
+++ 001-datatypes/main.js
@@ -0,0 +1,37 @@
+var a=1;
+window.a2=33;
+
+(function(){
+    console.log('Hello')
+    var aa = 2;
+})()
+
+let b=2;
+const c=3;
+
+// datatypes 8
+
+// primitive 
+const d=1; //number
+const e='hello'//string
+const f=true //boolean
+
+//special values
+const g = undefined //by default
+const h = null //nothing
+const i = NaN //unreal operation
+
+//objects
+const k={a:1}//object
+const l=[1,2,3]
+const m=function(){}
+
+
+//output
+console.log('Hello')
+console.warn('Hello')
+console.error('Hello')
+
+document.write('hello dom');
+const h1DOM = document.querySelector('h1')
+h1DOM.style.background='orange';
