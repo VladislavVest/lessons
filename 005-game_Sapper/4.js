e425161fc81c7e0d4dd87ed86802d8740b4cb7e3
VladislavVest <vladislavbrusencov@gmail.com>
2023-07-17 16:04:16 +0300

    scoreGame

---
+++ 005-game_Sapper/4.js
@@ -0,0 +1,93 @@
+const log = console.log;
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+const mines = [getRandomInt(10), getRandomInt(10), getRandomInt(10)];// random from 0, to 10
+log(mines)
+let life = 2;
+let score = 0;
+const setScore = () => document.querySelector('.score-number').innerHTML = score
+const setLife = () => document.querySelector('.life-number').innerHTML = life
+setLife()
+function check(n, el) {
+
+    mines.forEach((current) => {
+        if (current == n) {
+            el.classList.add('off')
+            life--
+            if (life == 0) gameOver();
+        } else score++
+    })
+
+    log('life', life)
+    setScore()
+    setLife() 
+    el.classList.add('touch')
+}
+function saveScore() {
+
+
+    const savedScoreJSON = localStorage.getItem('score')
+    let savedScoreArr = []
+    log(savedScoreJSON);
+    if (savedScoreJSON) {
+        try {
+            savedScoreArr = JSON.parse(savedScoreJSON)
+        } catch (error) {
+            log(error, 'cant parse JSON')
+        }
+    }
+    log(savedScoreArr)
+    savedScoreArr.push({
+        date: '12.12.1993',
+        score: 22
+    })
+    log(savedScoreArr);
+    const newSavedScoreJSON = JSON.stringify(savedScoreArr)
+    localStorage.setItem('score', newSavedScoreJSON)
+}
+function gameOver() {
+    saveScore()
+
+
+
+    alert('you are dead');
+    setTimeout(() => {
+        location.reload();
+    }, 1000)
+
+}
+
+//get container
+const containerDOM = document.querySelector('.container')
+log(containerDOM);
+for (let i = 0; i < 10; i++) {
+    containerDOM.innerHTML += `<button onclick="check(${i},this)">[]</button>`
+}
+
+
+
+
+// // Function arguments
+
+// function sum(a, b) {
+//     return a + b
+// }
+
+// const result = sum(3, 5)
+
+
+
+
+
+
+
+
+
+
+
+/* <button onclick="check(1)">[]</button> */
+
+
--- train/main.js
+++ train/main.js
@@ -1,44 +1,71 @@
 const log = console.log;
 
-
-
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-
-const minePosition = getRandomInt(10);// random from 0, to 10
-log(minePosition)
-
-
-
-
-
-
+const mines = [getRandomInt(10), getRandomInt(10), getRandomInt(10)];// random from 0, to 10
+log(mines)
+let life = 2;
+const setScore =()=>document.querySelector('.score-number').innerHTML=life
+setScore()
 function check(n, el) {
-    log(n)
-    if (minePosition == n) {
 
-        el.classList.add('off')
-        el.classList.add ('animate')
-        alert('you are dead');
-        setTimeout(function () {
-            location.reload();
-        }, 1000)
+    mines.forEach((current) => {
+        if (current == n) {
+            el.classList.add('off')
+            
+            life--
+            if (life == 0) {
+                alert('you are dead');
+                setTimeout(() => {
+                    location.reload();
+                }, 1000)
+                
+            }
 
-    }
-    el.classList.add('touch')
+
+        }
+    })
+
+    log('life', life)
+    setScore()  
+    el.classList.add('touch2')
 }
-
 
 
 //get container
 const containerDOM = document.querySelector('.container')
 log(containerDOM);
 for (let i = 0; i < 10; i++) {
-    containerDOM.innerHTML += `<button onclick="check(${i},this)">[]</button>`
-   
+    containerDOM.innerHTML += `<button onclick="check(${i},this)">[]</button>`  
+    
 }
+ 
+
+
+
+// // Function arguments
+
+// function sum(a, b) {
+//     return a + b
+// }
+
+// const result = sum(3, 5)
+
+
+
+
+
+
+
+
+
+
+
+/* <button onclick="check(1)">[]</button> */
+
+
 
 
 
