5ccff6b0d0d2d1f31ee98f00af64e99c56f17e75
VladislavVest <vladislavbrusencov@gmail.com>
2023-08-08 18:07:13 +0300

    BD

---
+++ 013-mongoDB/example.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const log = console.log;
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
+
+// mongoose.connect(`mongodb+srv://user:${process.env.DB_PASS}@cluster0-bpnma.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
+// // mongoose.connect('mongodb://127.0.0.1/ibanan', {
+//     // useCreateIndex: true,
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//     // useFindAndModify: false
+// })
+//     .then(() => log('\n DB Connected!'))
+//     .catch(err => log(`DB Connection Error: ${err.message}`));
